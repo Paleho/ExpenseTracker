32a4ec2abac5f4bac05eb0918acbaefc60801488
Sokratis Poutas <poutasok@gmail.com>
2023-05-06 14:25:56 +0300

    Wrap HeatMap in 3-month lengths and apply custom colorSet with saturated color

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
+import 'WrappedHeatMap.dart';
 import 'heatmap_example.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -22,30 +13,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Map<int, Color> myColorSet = <int, Color>{
-    0: Colors.green.shade50,
-    1: Colors.green.shade300,
-  };
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Center(child: Text('Expense Tracker App')),
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                // reverse: true,
-                child: HeatMap(
-                  // scrollable: true,
-                  colorsets: myColorSet,
-                ),
-              ),
-            ),
-          )
+          WrappedHeatMap(endDate: DateTime.now()),
         ],
       ),
       // body: HeatMapExample(),
---
+++ lib/WrappedHeatMap.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+
+class WrappedHeatMap extends StatelessWidget {
+  final DateTime endDate;
+  final Color maxColor = Colors.green.shade700;
+  final Color saturatedColor = Colors.red.shade900;
+  Map<int, Color>? myColorSet;
+
+  WrappedHeatMap({super.key, required this.endDate}) {
+    myColorSet = <int, Color>{
+      1: maxColor.withOpacity(0.1),
+      2: maxColor.withOpacity(0.2),
+      3: maxColor.withOpacity(0.3),
+      4: maxColor.withOpacity(0.4),
+      5: maxColor.withOpacity(0.5),
+      6: maxColor.withOpacity(0.6),
+      7: maxColor.withOpacity(0.7),
+      8: maxColor.withOpacity(0.8),
+      9: maxColor.withOpacity(0.9),
+      10: maxColor.withOpacity(1),
+      11: saturatedColor,
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime startDate = DateTime(endDate.year, endDate.month - 3, endDate.day);
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: HeatMap(
+          datasets: {
+            DateTime(2023, 5, 1): 1,
+            DateTime(2023, 5, 2): 8,
+            DateTime(2023, 5, 3): 8,
+            DateTime(2023, 5, 4): 10,
+            DateTime(2023, 4, 25): 2,
+            DateTime(2023, 4, 26): 0,
+            DateTime(2023, 4, 20): 5,
+            DateTime(2023, 5, 6): 20,
+            DateTime(2023, 3, 6): 100,
+          },
+          startDate: startDate,
+          endDate: endDate,
+          colorMode: ColorMode.color,
+          scrollable: true,
+          colorsets: myColorSet!,
+        ),
+      ),
+    );
+  }
+}
