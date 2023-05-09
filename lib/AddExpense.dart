6c459d0749ac55c7d2758b61bf6a754d43a555cc
Sokratis Poutas <poutasok@gmail.com>
2023-05-09 03:43:34 +0300

    AddExpense: add more button

--- lib/AddExpense.dart
+++ lib/AddExpense.dart
@@ -19,56 +19,96 @@ class _AddExpenseState extends State<AddExpense> {
   void createExpense() {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('New'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                hintText: '13.2',
-                icon: SizedBox(
-                  width: 30,
-                  child: Icon(Icons.euro_symbol_rounded),
-                ),
-              ),
-              controller: createExpenseAmountController,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                hintText: 'Lunch',
-                icon: Text(
-                  'Tag:',
-                  style: TextStyle(fontWeight: FontWeight.w500),
-                ),
-              ),
-              controller: createExpenseNameController,
-            ),
-          ],
-        ),
-        actionsAlignment: MainAxisAlignment.spaceEvenly,
-        actions: [
-          // Save
-          ElevatedButton(
-            onPressed: save,
-            style: ElevatedButton.styleFrom(
-              fixedSize: Size(74, 28),
-              backgroundColor: Color.fromARGB(200, 55, 55, 55),
-            ),
-            child: const Text('Save'),
-          ),
+      builder: (context) {
+        bool moreVisible = false;
+        IconData moreIcon = Icons.arrow_right;
 
-          // Cancel
-          ElevatedButton(
-            onPressed: cancel,
-            style: ElevatedButton.styleFrom(
-              fixedSize: Size(74, 28),
-              backgroundColor: Color.fromARGB(200, 173, 25, 23),
+        return StatefulBuilder(builder: (context, setState) {
+          return AlertDialog(
+            title: const Text('New'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Amount
+                TextField(
+                  decoration: const InputDecoration(
+                    hintText: '13.2',
+                    icon: SizedBox(
+                      width: 30,
+                      child: Icon(Icons.euro_symbol_rounded),
+                    ),
+                  ),
+                  controller: createExpenseAmountController,
+                ),
+                // Tag
+                TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Lunch',
+                    icon: Text(
+                      'Tag:',
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  controller: createExpenseNameController,
+                ),
+                const SizedBox(height: 5),
+                // More button
+                SizedBox(
+                  height: 22,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        moreIcon = moreVisible
+                            ? Icons.arrow_right
+                            : Icons.arrow_drop_down;
+                        moreVisible = !moreVisible;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(200, 130, 130, 130),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('More'),
+                        SizedBox(
+                          width: 12,
+                          child: Icon(moreIcon),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                if (moreVisible) Text('more details here'),
+              ],
             ),
-            child: const Text('Cancel'),
-          ),
-        ],
-      ),
+            actionsAlignment: MainAxisAlignment.spaceEvenly,
+            actions: [
+              // Save
+              ElevatedButton(
+                onPressed: save,
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(74, 28),
+                  backgroundColor: Color.fromARGB(200, 55, 55, 55),
+                ),
+                child: const Text('Save'),
+              ),
+
+              // Cancel
+              ElevatedButton(
+                onPressed: cancel,
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(74, 28),
+                  backgroundColor: Color.fromARGB(200, 173, 25, 23),
+                ),
+                child: const Text('Cancel'),
+              ),
+            ],
+          );
+        });
+      },
     );
   }
 
